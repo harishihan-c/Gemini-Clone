b339cb8dc9fedba7433c41776c8e4a1091792325
harishihan-c <harishihan@outlook.com>
2025-08-15 22:50:19 +0530

    Setup: Initial file structure created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import SideBar from './components/SideBar/SideBar'
+import Main from './components/Main/Main'
+
+
+function App() {
+
+
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import menu from '../assets/menu-burger.png'
+import add from '../assets/add.png'
+
+const assets = {
+    menu,
+    add
+}
