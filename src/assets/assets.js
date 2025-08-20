51c70dfed0e9bb4e9c449a63f84c3e819bb71f60
harishihan-c <harishihan@outlook.com>
2025-08-20 13:30:50 +0530

    feat(assets): New images added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,11 @@ import chat from '../assets/bubble-chat.png'
 import setting from '../assets/setting.png'
 import question from '../assets/question.png'
 import history from '../assets/history.png'
+import robot from '../assets/robot.png'
+import compass from "../assets/compass.png"
+import message from '../assets/comment.png'
+import bulb from '../assets/lightbulb.png'
+import code from '../assets/code.png'
 
 export const assets = {
     menu,
@@ -11,5 +16,10 @@ export const assets = {
     chat,
     setting,
     question,
-    history
+    history,
+    robot,
+    compass,
+    code,
+    bulb,
+    message
 }
