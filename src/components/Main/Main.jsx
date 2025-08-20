0db84c2847ee9c33919d328ac2d77b53b1b016db
harishihan-c <harishihan@outlook.com>
2025-08-20 17:16:28 +0530

    feat: Main section UI completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
 function Main() {
   return (
-    <div>
-      
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.robot} />
+      </div>
+
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello Kate</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
+            <img src={assets.compass} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
+            <img src={assets.bulb} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
+            <img src={assets.message} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
+            <img src={assets.code} alt="" />
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt" />
+            <div>
+              <img src={assets.gallary} alt="" />
+              <img src={assets.voice} alt="" />
+              <img src={assets.send} alt="" />
+            </div>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque atque provident quos?</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
