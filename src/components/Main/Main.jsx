7e35a5d79779f51616fa63c400b1dcdc5f425c1f
harishihan-c <harishihan@outlook.com>
2025-08-22 16:13:19 +0530

    ft: logic added for showing the blank page after insert a input

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,22 @@
-import React from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../Context/context";
+import generate from "../../Config/config";
 
 function Main() {
+  const { 
+    setInput,
+    input, 
+    onSent, 
+    setResult, 
+    result, 
+    setLoading,
+    recentPrompt,
+    setRecentPrompt,
+    response,
+    } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -11,39 +25,80 @@ function Main() {
       </div>
 
       <div className="main-container">
-        <div className="greet">
-          <p><span>Hello Kate</span></p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
-            <img src={assets.compass} alt="" />
+        {!result ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello Kate</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aspernatur, fugit.
+                </p>
+                <img src={assets.compass} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aspernatur, fugit.
+                </p>
+                <img src={assets.bulb} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aspernatur, fugit.
+                </p>
+                <img src={assets.message} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aspernatur, fugit.
+                </p>
+                <img src={assets.code} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="reult-title">
+              <img src="" alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <p>{response}</p>
+            
           </div>
-          <div className="card">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
-            <img src={assets.bulb} alt="" />
-          </div>
-          <div className="card">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
-            <img src={assets.message} alt="" />
-          </div>
-          <div className="card">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fugit.</p>
-            <img src={assets.code} alt="" />
-          </div>
-        </div>
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt" />
+            <input
+              type="text"
+              placeholder="Enter a prompt"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+            />
             <div>
               <img src={assets.gallary} alt="" />
               <img src={assets.voice} alt="" />
-              <img src={assets.send} alt="" />
+              <img
+                onClick={() => {
+                  onSent();
+                }}
+                src={assets.send}
+                alt=""
+              />
             </div>
           </div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque atque provident quos?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
+            atque provident quos?
+          </p>
         </div>
       </div>
     </div>
